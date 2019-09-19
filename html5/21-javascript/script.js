bcd31da686405c51937dca1167a9d6f641d12910
Ranie <raniesilvarj@gmail.com>
2019-09-19 16:24:40 -0300

    Add files via upload

---
+++ html5/21-javascript/script.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("external-1");
+var y = document.getElementById("external-2");
+
+x.innerHTML = "This header was add by an external Javascript file.";
+y.innerHTML = "This text was add by an external Javascript file.";
+
+
+x.style.color = "royalblue";
+y.style.color = "seagreen";
