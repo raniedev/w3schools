4fecaf1db56d80dfa4204cc2477211b056d7a977
Ranie <raniedev@gmail.com>
2026-03-22 03:33:05 -0300

    RegExp: uso de Flags, Classes, Metachars e Assertions.

--- JavaScript/js/08-math-regex.js
+++ JavaScript/js/08-math-regex.js
@@ -117,3 +117,244 @@ function numAleatorio(min, max){
 }
 
 console.log(numAleatorio(5, 25));
+
+/**
+ * RegEx - Regular Expressions
+ * 
+ * É uma sequência de caracteres que formam um padrão de pesquisa
+ * Regex é um atalho de uma expressão regular
+ */
+
+//Um texto envolto de duas barras com i de case-insensitive, indica uma pesquisa que pouco se importa com caixa alta/baixa
+let lema = "Ordem Territorial, Progresso, ordem social e programa nacional. Matas verdejantes, Céu azul celeste e aurora de ouro";
+let search = lema.search(/ORDEM/i);
+let rslt = Math.sign(search);
+
+console.log(rslt >= 0 ? "Encontrou" : "Não Encontrou");
+
+//Substituindo com replace()
+let trocar = lema.replace(/ORDEM/i, "Desordem");
+console.log(trocar);
+
+//replaceAll()
+let cancao = "Amor, meu querido amor.";
+let regex = /amor/gi;
+let trocarTodos = cancao.replaceAll(regex, "ódio");
+console.log(trocarTodos);
+
+//split(), retorna um array com os resultados
+
+
+//RexExp Alternation (OR), 
+let rslt2 = (lema.search(/verde|amarelo|laranja/g));
+console.log(rslt2);
+
+/**
+ * /d = substring matches
+ * /g = global match
+ * /i = case-insensitive
+ * /m = multiline matching
+ * /s = allows (. dot) to match line terminators 
+ * /u = unicode
+ * /v = an upgrade to the /u flag for better Unicode support 
+ * /y = performs a "sticky" search
+ * 
+**/
+
+// Determina qual deve ser o início AND o final de uma string
+const barraD = /(can)(cao)/d;
+let palavra1 = "cancao";
+let palavra2 = "cançao";
+let palavra3 = "canção";
+
+let match1 = palavra1.match(barraD);
+let match2 = palavra2.match(barraD);
+let match3 = palavra3.match(barraD);
+
+console.log("Regex Flag /d");
+console.log(match1);
+console.log(match2); //null
+console.log(match3); //null
+
+// O sinalizador /s permite que o metacaractere . (ponto) corresponda aos caracteres 
+// de nova linha (\n), além de qualquer outro caractere.
+const barraS = /Line./gs;
+let palavra4 = "Line\nLine.";
+
+let match4 = palavra4.match(barraS);
+console.log("Regex Flag /s");
+console.log(match4);
+
+// O sinalizador /y executa uma pesquisa "fixa"
+const barraY = /\w+/y; //The meaning of /\w+/ is "mach any word".
+let palavra5 = "abc def ghi";
+barraY.lastIndex = 4;
+let match5 = palavra5.match(barraY);
+console.log("Regex Flag /y");
+console.log(match5);
+
+// the /u flag enables full Unicode support in a regular expression.
+const barraU = /\u{04DC0}/u;
+let palavra6 = "䷀";
+let match6 = barraU.test(palavra6);
+console.log("Regex Flag /u");
+console.log(match6);
+
+// The /v flag is an upgrade to the /u flag for better Unicode support
+const barraV = /\p{Emoji}/v;
+let palavra7 = "Hello 😄";
+let match7 = barraV.test(palavra7);
+console.log("Regex Flag /v");
+console.log(match7);
+
+// The multiline property returns true if the /m flag is set.
+const barraM = /W3Schools/m;
+let match8 = barraM.multiline;
+console.log("Regex Flag /m");
+console.log(match8);
+
+/**
+ * 
+ * RexExp Metacaracteres
+ * \d = dígito/número
+ * \D = não dígito
+ * \w = palavras (seja letra, dígito e underscore)
+ * \W = não palavra
+ * \s = espaço
+ * \S = não espaço
+ * \n = quebra de linha
+ * \t = tab
+ * \ddd = octal number ddd
+ * \xhh = hexadecimal number hh
+ * \uhhhh = hex number hhhh
+ */
+
+let texto = "Vendo este carro por R$ 39000, este é o CARRO! É extremamente rápido.";
+const digito = /\d/g;
+const letra = /\w/g;
+const espaco = /\s/g;
+
+//match() armazena o que achar em um array
+let resultado = texto.match(digito); //todos os dígitos
+console.log(resultado);
+
+let resultado2 = texto.match(letra); //todos as palavras
+console.log(resultado2);
+
+// RegExp Quantificadores
+// + um ou mais
+// * zero ou mais
+// ? zero ou um
+const umaOuMais = /a+/i;
+const zeroOuMais = /carro*/ig;
+const zeroOuUma = /este?/i;
+const numOcorrencias = /0{3}/i;
+
+console.log(texto.match(umaOuMais));
+console.log(texto.match(zeroOuMais));
+console.log(texto.match(zeroOuUma));
+console.log(texto.match(numOcorrencias));
+
+// RegExp Assertions
+// ^ começo da string
+// $ final da string
+// \b começo ou final da string
+// \B negação do começo ou final da string
+// (?=...) string subsequente
+// (?!...) negação de string subsequente
+// (?<=...) string anterior
+// (?<!...) negação de string anterior
+
+let hino = "Ouviram do Ipiranga as margens plácidas";
+let comeco = /^Ouviram/;
+let final = /plácidas$/;
+
+//test(), testa se há na string ou não o regex passado
+console.log(comeco.test(hino));
+console.log(final.test(hino));
+
+let musica = "Amor, eu te amo"
+let comecoFim = /\bamor/i;
+let musica2 = "Amo muito, meu amor"
+console.log(comecoFim.test(musica));
+console.log(comecoFim.test(musica2));
+
+let tamanho = "10px 20px";
+let pixel = /\d+(?=px)/g; //Ao menos um dígito ou mais de um, seguidos de px (pesquisa global)
+
+console.log(tamanho.match(pixel));
+
+//exec(), retorna um iterador de resultados
+let texto3 = "E então ele acordou e saiu para olhar o nascer do sol";
+let resultado3 = /e/.exec(texto3);
+console.log(resultado3);
+
+//matchAll()
+let todos = tamanho.matchAll('px');
+for(let t of todos){
+    console.log(t);
+}
+
+// RegExp Classes de Caracteres
+// [a], caractere exato
+// [^a], negação do caractere passado
+// [abc], vários caracteres, significa: a, b ou c 
+// [^abc], negação dos caracteres passados
+// [0-9], dígitos de 0 até 9
+// [^0-9], negação de dígitos de 0 até 9
+// [a-z], letras minúsculas de a até z
+// [^a-z], negação de letras minúsculas de a até z
+// [A-Z], letras maiúsculas de A até Z
+// [A-Z], negação de letras maiúsculas de A até Z
+let texto2 = "BRASIL foi 5 vezes campeão da copa do mundo.";
+let minuscula = /[a-z]/;
+let maiuscula = /[A-Z]/;
+let num = /[0-9]/;
+
+console.log(texto2.match(maiuscula));
+console.log(texto2.match(minuscula));
+console.log(texto2.match(num));
+
+// RegExp Lookahead
+// x(?=y) combina "x" se "x" é seguido pela string "y"
+// x(?!y) é a forma negativa
+let texto4 = "Acessando o Twitter Videos";
+let lookahead1 = /Twitter(?= Videos)/;
+let resultado4 = lookahead1.test(texto4);
+console.log(resultado4);
+
+// x(<=y) combina "x" se "x" é precedido pela string "y"
+// x(<!y) é a forma negativa
+let texto5 = "Acessando o Twitter Videos";
+let lookahead2 = /Videos(?= Twitter)/;
+let resultado5 = lookahead2.test(texto4);
+console.log(resultado4);
+
+// RegExp Groups
+// (haha) captures a group of characters
+// (haha)+ matches zero or more occurences of the group
+let texto6 = "Haha, haha, haha.";
+const pattern = /(haha)+/;
+let resultado6 = texto6.match(pattern);
+
+// Capturando grupos (?<n>)
+const texto7 = "Nome: Pedro Alvares Cabral";
+
+// Using named capturing groups
+// Seria como criar rótulos para neste caso 3 palavras (w+), e depois da 1 e 2 palavra tiver um espaço
+const lookahead3 = /(?<firstName>\w+) (?<middleName>\w+) (?<lastName>\w+)/;
+const resultado7 = texto7.match(lookahead3);
+
+//Esses "rótulos" servem como nomes para depois retornar o que estiver dentro
+let fName = resultado7.groups.firstName;
+let mName = resultado7.groups.middleName;
+let lName = resultado7.groups.lastName;
+
+console.log(`${fName} ${mName} ${lName}`);
+
+// (?flags:pattern) enables the flags only for the pattern in the group
+let texto8 = "W3Schools tutorials.";
+const lookahead4 = /(?i:w3schools) tutorials/;
+// Returns true:
+let resultado8 = lookahead4.test(texto8);
+console.log(resultado8);
